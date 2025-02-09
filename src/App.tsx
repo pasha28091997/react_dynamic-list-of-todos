a0b6a20c3794e027cdcc7c83358812639e684f13
Pasha Shchukin <pasha4320692@gmail.com>
2025-02-09 16:50:35 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,51 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loader, setLoader] = useState<boolean>(true);
+  const [statusFilter, setStatusFilter] = useState('all');
+  const [query, setQuery] = useState('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    setLoader(true);
+
+    getTodos()
+      .then(setTodos)
+      .finally(() => setLoader(false));
+  }, []);
+
+  const filterByStatus = (todo: Todo) => {
+    if (statusFilter === 'all') {
+      return true;
+    }
+
+    if (statusFilter === 'active') {
+      return !todo.completed;
+    }
+
+    if (statusFilter === 'completed') {
+      return todo.completed;
+    }
+
+    return true;
+  };
+
+  const filterByQuery = (todo: Todo) =>
+    todo.title.toLowerCase().includes(query.toLowerCase());
+
+  const filteredTodos = todos.filter(
+    todo => filterByStatus(todo) && filterByQuery(todo),
+  );
+
+  const handleClear = () => {
+    setQuery('');
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +60,32 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                status={statusFilter}
+                query={query}
+                onStatusChange={setStatusFilter}
+                onQueryChange={setQuery}
+                onClear={handleClear}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loader ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  onShow={(todo: Todo) => setSelectedTodo(todo)}
+                  onHide={() => setSelectedTodo(null)}
+                  selectedTodoId={selectedTodo ? selectedTodo.id : null}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      <TodoModal todo={selectedTodo} onClose={() => setSelectedTodo(null)} />
     </>
   );
 };
